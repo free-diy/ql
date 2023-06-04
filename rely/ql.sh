443a4658db7207cdde8ef75acd50354d212947c7
free-diy <71208560+free-diy@users.noreply.github.com>
2023-06-04 15:56:19 +0800

    Update ql.sh

--- rely/ql.sh
+++ rely/ql.sh
@@ -29,14 +29,17 @@ npm config set registry https://mirrors.huaweicloud.com/repository/npm/
 
 apk update
 
-
-TIME l "安装yarn"
-npm install -g yarn --force
+apk add vim nano
 
 TIME l "安装依赖"
 pnpm install -g pnpm js-base64 qrcode-terminal silly-datetime
 TIME l "安装依赖"
 npm install -g npm date-fns axios png-js crypto-js ds md5 ts-md5 tslib download @types/node requests tough-cookie jsdom tunnel fs ws form-data js-base64 node-rsa jsencrypt got silly-datetime cheerio moment node-jsencrypt
+TIME l "安装yarn"
+npm install -g yarn --force
+TIME l "安装依赖typescript"
+yarn add typescript
+
 TIME l "升级pip"
 pip3 install --upgrade pip
 TIME l "升级setuptools"
