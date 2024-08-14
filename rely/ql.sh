3d9a17b9ead653ba41abdc72b2389e8574273d71
free-diy <71208560+free-diy@users.noreply.github.com>
2024-08-14 09:28:38 +0800

    Update ql.sh

--- rely/ql.sh
+++ rely/ql.sh
@@ -1,59 +1,42 @@
 #!/usr/bin/env bash
+#依赖安装，运行一次就好
 #
+npm_ver=$(pnpm -v | awk -F. '{print $1}')
+if [[ $npm_ver -ge 7 ]]; then
+    export PNPM_HOME="/root/.local/share/pnpm"
+    export PATH="$PNPM_HOME:$PATH"
+fi
 
-TIME() {
-[[ -z "$1" ]] && {
-	echo -ne " "
-} || {
-     case $1 in
-	r) export Color="\e[31;1m";;
-	g) export Color="\e[32;1m";;
-	b) export Color="\e[34;1m";;
-	y) export Color="\e[33;1m";;
-	z) export Color="\e[35;1m";;
-	l) export Color="\e[36;1m";;
-      esac
-	[[ $# -lt 2 ]] && echo -e "\e[36m\e[0m ${1}" || {
-		echo -e "\e[36m\e[0m ${Color}${2}\e[0m"
-	 }
-      }
-}
-echo
-TIME l "安装依赖..."
-echo
-TIME y "安装依赖需要时间，请耐心等待!"
+echo -e "安装脚本所需依赖，不一定一次全部安装成功，请自己检查\n"
+echo -e "开始安装............\n"
 
-echo
-
-apk update
-
-apk add vim nano g++ make --no-cache
-
-TIME l "安装依赖"
-npm config set registry https://registry.npmmirror.com
-npm install -g npm date-fns axios png-js crypto-js ds md5 ts-md5 tslib download @types/node requests tough-cookie jsdom tunnel fs ws form-data js-base64 node-rsa jsencrypt got silly-datetime cheerio moment node-jsencrypt iconv-lite global-agent yarn --force
-
-pnpm add -g pnpm
-
+#apk add g++ make pixman-dev pango-dev cairo-dev pkgconf --no-cache
+apk add g++ make --no-cache
+pnpm config set registry https://registry.npmmirror.com
 pnpm install -g
-
-pnpm install -g pnpm ds png-js date-fns axios@0.27.2 crypto-js ts-md5 tslib @types/node request jsdom moment cheerio tough-cookie https-proxy-agent js-base64 qrcode-terminal silly-datetime global-agent
-
-TIME l "升级pip"
-pip3 install --upgrade pip
-TIME l "升级setuptools"
-pip3 install --upgrade setuptools
-
-TIME l "安装pip依赖"
-pip3 install requests download jieba lxml gevent b4 bs4 zhon moment aiohttp pycryptodome PyExecJS
-
-cd /ql/data/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && npm i && npm i -S ts-node typescript @types/node date-fns axios png-js canvas --build-from-source
-cd /ql
-apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && cd scripts && npm install canvas --build-from-source
-TIME l "安装python3"
-cd /ql
-apk add python3 zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev
-echo
-TIME g "依赖安装完毕..."
-echo
-exit 0
+pnpm install -g adler-32
+pnpm install -g png-js
+pnpm install -g date-fns
+pnpm install -g axios@1.6.7
+pnpm install -g crypto-js
+pnpm install -g ts-md5
+pnpm install -g tslib
+pnpm install -g global-agent
+pnpm install -g @types/node
+pnpm install -g request
+pnpm install -g jsdom
+pnpm install -g crc
+pnpm install -g qs
+pnpm install -g moment
+pnpm install -g cheerio
+pnpm install -g dotenv
+pnpm install -g got@11.8.6
+pnpm install -g tough-cookie
+pnpm install -g https-proxy-agent@7.0.2
+pnpm install -g http-cookie-agent
+pnpm install -g console-table-printer@2.12.0
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple/ jieba
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple/ requests
+rm -rf /usr/local/pnpm-global/5/node_modules/.pnpm/canvas*
+rm -rf /root/.local/share/pnpm/global/5/.pnpm/canvas*
+echo -e "\n所需依赖安装完成，请检查有没有报错，可尝试再次运行"
